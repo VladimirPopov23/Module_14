bee670a1e621b63353bb0b852707cdafbada6e4a
Vladimir <volodyaklever@gmail.com>
2025-01-21 20:53:34 +0300

    module_14_2.py
    21.01.2025 Задача "Средний баланс пользователя":

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# module_14_1.py
-# 19.01.2025 Задача "Первые пользователи"
+# module_14_2.py
+# 21.01.2025 Задача "Средний баланс пользователя":
 
 import sqlite3
 
@@ -33,10 +33,35 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 #     cursor.execute("DELETE FROM Users WHERE id = ?", (f"{i}",))
 
 # Сделайте выборку ви выведете в консоль
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
-users = cursor.fetchall()
-for user in users:
-    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+# cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+# users = cursor.fetchall()
+# for user in users:
+#     print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+# 1. Удалите из базы данных not_telegram.db запись с id = 6.:
+# cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+
+# 2. Подсчитать общее количество записей.
+# cursor.execute("SELECT COUNT(*) FROM Users")
+# total_users = cursor.fetchall()[0]
+# print(f"Общее количество записей: {total_users[0]}")
+
+# 3. Посчитать сумму всех балансов.
+# cursor.execute("SELECT SUM(balance) FROM Users")
+# all_balance = cursor.fetchall()[0]
+# print(f"Сумма всех балансов: {all_balance[0]}")
+
+# 4. Вывести в консоль средний баланс всех пользователей.
+# 4.1.
+cursor.execute("SELECT AVG(balance) FROM Users")
+average_balance = cursor.fetchall()[0]
+print(f"Средний баланс всех пользователей: {average_balance[0]}")
+# 4.2.
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchall()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balance = cursor.fetchall()[0]
+print(f"Средний баланс всех пользователей: {all_balance[0] / total_users[0]}")
 
 connection.commit()
 connection.close()
