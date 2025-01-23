48436dd328ee0971aaa454b5ce385af5d7287883
Vladimir <volodyaklever@gmail.com>
2025-01-23 21:53:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7917007751:AAHCWYAZqxlK4ybYffV-q9F7W8ezfVBUtSY'
+api = 'код'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
