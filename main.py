5a31a6acf794a5b914432481667e4d7fe2f3468f
Vladimir <volodyaklever@gmail.com>
2025-01-23 21:47:41 +0300

    module_14_3.py
    23.01.2025 Задача "Витамины для всех!"

--- main.py
+++ main.py
@@ -1,67 +1,113 @@
-# module_14_2.py
-# 21.01.2025 Задача "Средний баланс пользователя":
+# module_14_3.py
+# 23.01.2025 Задача "Витамины для всех!"
 
-import sqlite3
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
 
-connection = sqlite3.connect("not_telegram.db")
-cursor = connection.cursor()
+api = '7917007751:AAHCWYAZqxlK4ybYffV-q9F7W8ezfVBUtSY'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users(
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER,
-balance INTEGER NOT NULL
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Рассчитать'), KeyboardButton(text='Информация')],
+        [KeyboardButton(text='Купить')],
+    ], resize_keyboard=True
 )
-''')
 
-cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+inline_menu = InlineKeyboardMarkup(row_width=4)
+Product1 = InlineKeyboardButton(text='Продукт 1', callback_data='product_buying')
+Product2 = InlineKeyboardButton(text='Продукт 2', callback_data='product_buying')
+Product3 = InlineKeyboardButton(text='Продукт 3', callback_data='product_buying')
+Product4 = InlineKeyboardButton(text='Продукт 4', callback_data='product_buying')
+inline_menu.row(Product1)
+inline_menu.insert(Product2)
+inline_menu.insert(Product3)
+inline_menu.insert(Product4)
 
-# Заполнение базы данных
-# for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i}",
-#                                                                                              f"example{i}@gmail.com",
-#                                                                                              f"{i}0", 1000))
 
-# Обновите balance у каждой 2ой записи начиная с 1ой на 500
-# for i in range(1, 11, 2):
-#     cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, f"{i}"))
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
 
-# Удалите каждую 3ую запись в таблице начиная с 1ой:
-# for i in range(1, 11, 3):
-#     cursor.execute("DELETE FROM Users WHERE id = ?", (f"{i}",))
 
-# Сделайте выборку ви выведете в консоль
-# cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
-# users = cursor.fetchall()
-# for user in users:
-#     print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for number in range(1, 5):
+        with open(f'files/{number}.jpg', 'rb') as img:
+            await message.answer_photo(img, f'Название: Product{number} | Описание: описание {number} | Цена: {number * 100}')
+    await message.answer('Выберите продукт для покупки:', reply_markup=inline_menu)
 
-# 1. Удалите из базы данных not_telegram.db запись с id = 6.:
-# cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
 
-# 2. Подсчитать общее количество записей.
-# cursor.execute("SELECT COUNT(*) FROM Users")
-# total_users = cursor.fetchall()[0]
-# print(f"Общее количество записей: {total_users[0]}")
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('Информация о боте!')
 
-# 3. Посчитать сумму всех балансов.
-# cursor.execute("SELECT SUM(balance) FROM Users")
-# all_balance = cursor.fetchall()[0]
-# print(f"Сумма всех балансов: {all_balance[0]}")
 
-# 4. Вывести в консоль средний баланс всех пользователей.
-# 4.1.
-cursor.execute("SELECT AVG(balance) FROM Users")
-average_balance = cursor.fetchall()[0]
-print(f"Средний баланс всех пользователей: {average_balance[0]}")
-# 4.2.
-cursor.execute("SELECT COUNT(*) FROM Users")
-total_users = cursor.fetchall()[0]
-cursor.execute("SELECT SUM(balance) FROM Users")
-all_balance = cursor.fetchall()[0]
-print(f"Средний баланс всех пользователей: {all_balance[0] / total_users[0]}")
+# @dp.message_handler(text='Рассчитать')
+# async def main_menu(message):
+#     await message.answer('Выберите опцию:', reply_markup=inline_menu)
 
-connection.commit()
-connection.close()
+# @dp.callback_query_handler(text='formulas')
+# async def get_formulas(call):
+#     await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+#     await call.answer()
+#
+#
+# @dp.callback_query_handler(text='calories')
+# async def set_age(call):
+#     await call.message.answer('Введите свой возраст:')
+#     await call.answer()
+#     await UserState.age.set()
+#
+#
+# @dp.message_handler(text='Информация')
+# async def inform(message):
+#     await message.answer('Информация о боте!')
+#
+#
+# @dp.message_handler(state=UserState.age)
+# async def set_growth(message, state):
+#     await state.update_data(age=message.text)
+#     await message.answer('Введите свой рост:')
+#     await UserState.growth.set()
+#
+#
+# @dp.message_handler(state=UserState.growth)
+# async def set_weight(message, state):
+#     await state.update_data(growth=message.text)
+#     await message.answer('Введите свой вес:')
+#     await UserState.weight.set()
+#
+#
+# @dp.message_handler(state=UserState.weight)
+# async def set_handler(message, state):
+#     await state.update_data(weight=message.text)
+#     data = await state.get_data()
+#     await message.answer(
+#         f"Ваша норма калорий {(10 * int(data['weight'])) + (6.25 * int(data['growth'])) - (5 * int(data['age'])) + 5}")
+#     await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=start_menu)
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
